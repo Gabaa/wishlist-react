9563a0153629ae965caba3505a9cf753b52aa4bb
Mikkel Erfurth Gaba <201809037@post.au.dk>
2020-11-29 20:47:00 +0100

    Added basic UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { makeStyles, Paper } from '@material-ui/core';
+import React from 'react';
+import Wish from './Wish';
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    backgroundColor: "#dddddd",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    textAlign: "center",
+  },
+  content: {
+    width: "50vw",
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
+  const wishes = ["En ny computer", "En endnu mere ny computer"];
+
   return (
-    <div>
-      Hello, World!
+    <div className={classes.root}>
+      <Paper>
+        <header className={classes.header}>
+          <h1>Mikkels ønskeseddel</h1>
+        </header>
+        <div className={classes.content}>
+          {wishes.map(wish => {
+            return (
+              <Wish wish={wish} />
+            );
+          })}
+        </div>
+      </Paper>
     </div>
   );
 }
